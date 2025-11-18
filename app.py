3fbc2a50f32c2ea68376c7575c1f7c7355af97cf
Andrii Sherstiuk <andrii.sherstiuk@dxc.com>
2025-11-18 11:17:04 +0200

    Add good code

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-VERY_LONG_CONSTANT_NAME_TO_DEMONSTRATE_LINE_LENGTH_VIOLATION = "This is a very long string that Pylint will flag as C0301 (Line too long)"
+VERY_LONG_CONSTANT_NAME_TO_DEMONSTRATE_LINE_LENGTH_VIOLATION = (
+    "This is a very long string that Pylint will flag as C0301 "
+    "(Line too long)"
+)
+
 
 def calculate_something(a, b):
     c = a + b
-    unused_variable = 100
     return c
 
+
 class MyDataProcessor:
-    
     def __init__(self, data):
         self.data = data
-        
-    def process(self):
-        Processed_data = [x * 2 for x in self.data]
-        return Processed_data
 
-if __name__ == "__main__":
+    def process(self):
+        processed_data = [x * 2 for x in self.data]
+        return processed_data
+
+    def count(self):
+        return len(self.data)
+
+
+def main():
     result = calculate_something(5, 7)
     processor = MyDataProcessor([1, 2, 3])
     final_result = processor.process()
     print(f"Result: {result}, Final: {final_result}")
+
+
+if __name__ == "__main__":
+    main()
